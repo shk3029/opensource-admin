fd2307c493f992c0b2d5efad6967af18f1c14fa1
jongseon <shc.jsjeong@gmail.com>
2022-07-29 09:53:30 +0900

    Initial commit

---
+++ src/main/java/com/opensource/admin/OpensourceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpensourceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpensourceAdminApplication.class, args);
+    }
+
+}
